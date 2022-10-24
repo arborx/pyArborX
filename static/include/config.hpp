014f5d8857844968b23d859e2b537710a64b34a8
root <jakob.bludau@tum.de>
2022-10-24 20:11:30 +0000

    interoperatbility with std::vector

---
+++ static/include/config.hpp
@@ -0,0 +1,13 @@
+#ifndef PYARBORX_CONFIG_H
+#define PYARBORX_CONFIG_H
+
+#include <ArborX_Point.hpp>
+#include <ArborX_Box.hpp>
+#include <ArborX_Sphere.hpp>
+
+#include <pybind11/stl.h>
+#include <pybind11/stl_bind.h>
+
+PYBIND11_MAKE_OPAQUE(std::vector<ArborX::Point>);
+PYBIND11_MAKE_OPAQUE(std::vector<ArborX::Box>);
+#endif
